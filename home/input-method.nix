5665a235d0ddda3736861d1b476b669a0a87db6a
Koki Watanabe <kouki.11235813@gmail.com>
2025-10-18 00:52:35 +0900

    input-method: Wayland環境変数を追加

--- home/input-method.nix
+++ home/input-method.nix
@@ -8,6 +8,13 @@
       fcitx5-mozc
       fcitx5-gtk
       fcitx5-configtool
+      kdePackages.fcitx5-qt
     ];
+    fcitx5.waylandFrontend = true;
+  };
+
+  home.sessionVariables = {
+    MOZ_ENABLE_WAYLAND = "1";
+    XDG_SESSION_TYPE = "wayland";
   };
 }
